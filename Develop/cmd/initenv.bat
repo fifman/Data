ffcdc837d78031765d584eea29f0d100fdfcc87d
fifman <1264380449@qq.com>
2016-10-31 10:28:39 +0800

    new

--- Develop/cmd/initenv.bat
+++ Develop/cmd/initenv.bat
@@ -17,8 +17,8 @@ choco install mingw
 cd .\vimfiles\bundle\vimproc.vim
 mingw32-make -f make_mingw64.mak
 
-cd %USERPROFILE%/vimfiles/bundle/YouCompleteMe
-install.py --clang-completer --tern-completer
+:: cd %USERPROFILE%/vimfiles/bundle/YouCompleteMe
+:: install.py --clang-completer --tern-completer
 
 rem install rename of perl
 git clone https://github.com/subogero/rename.git
@@ -28,13 +28,12 @@ cd ..
 rm -rf rename
 
 rem epub reader
-choco install fbreader
+rem choco install fbreader
 
 rem install data git repository
-mkdir D:\00Data
-D:
-cd D:\00Data
-git init
-git remote add github git@github.com:fifman/Data.git
-git pull github master
-git config --global credential.helper wincred
+:: mkdir D:\00Data
+:: cd D:\00Data
+:: git init
+:: git remote add github git@github.com:fifman/Data.git
+:: git pull github master
+:: git config --global credential.helper wincred
